06f4641e8153492d4ed1106b9670ddd072d7d239
Kiril Vladimirov <kiril@vladimiroff.org>
2013-06-28 19:36:46 +0300

    Add arithmetic functions over Vectors in utils

--- utils.go
+++ utils.go
@@ -23,14 +23,53 @@ func Rotated(vector *Vector, angle_degrees float64) *Vector {
 }
 
 // GetAngleBetween returns the angle between two Vectors in float64
-func GetAngleBetween(first *Vector, second *Vector) float64 {
+func GetAngleBetween(first, second *Vector) float64 {
 	cross := first.X*second.Y - first.Y*second.X
 	dot := first.X*second.X + first.Y*second.Y
 	return math.Atan2(cross, dot) / (math.Pi / 180)
 }
 
 // GetDistance returns the distance between two Vectors in float64
-func GetDistance(first *Vector, second *Vector) float64 {
+func GetDistance(first, second *Vector) float64 {
 	return math.Sqrt(math.Pow(first.X-second.X, 2) + math.Pow(first.Y-second.Y, 2))
 }
 
+// Collect creates new Vector and sets its X and Y to the sum of these in first and second
+func Collect(first, second *Vector) *Vector {
+	return New(first.X+second.X, first.Y+second.Y)
+}
+
+// Collect returns new Vector adding given value to first's X and Y
+func CollectToFloat64(vector *Vector, value float64) *Vector {
+	return New(vector.X+value, vector.Y+value)
+}
+
+// Add creates a new Vector and sets its X and Y to the difference first-second
+func Sub(first, second *Vector) *Vector {
+	return New(first.X-second.X, first.Y-second.Y)
+}
+
+// Div returns a new Vector, taking given value from first's X and Y
+func SubToFloat64(vector *Vector, value float64) *Vector {
+	return New(vector.X-value, vector.Y-value)
+}
+
+// Mul returns a new Vector by multiplication of first and second
+func Mul(first, second *Vector) *Vector {
+	return New(first.X*second.X, first.Y*second.Y)
+}
+
+// Div returns a new Vector using multiplication of first's X and Y by given value
+func MulToFloat64(vector *Vector, value float64) *Vector {
+	return New(vector.X*value, vector.Y*value)
+}
+
+// Div returns a new Vector by dividing first and second
+func Div(first, second *Vector) *Vector {
+	return New(first.X/second.X, first.Y/second.Y)
+}
+
+// Div returns a new Vector by dividing first's X and Y by given value
+func DivToFloat64(vector *Vector, value float64) *Vector {
+	return New(vector.X/value, vector.Y/value)
+}
--- utils_test.go
+++ utils_test.go
@@ -15,7 +15,7 @@ package vec2d
 
 import "testing"
 
-func TestRotated(t *testing.T) {
+func TestUtilsRotated(t *testing.T) {
 	vector := New(2.0, 4.0)
 	result := Rotated(vector, 2.0)
 
@@ -24,11 +24,11 @@ func TestRotated(t *testing.T) {
 	}
 
 	if vector == result {
-		t.Error("Vector.Rotated did not return a copy object")
+		t.Error("Rotated did not return a copy object")
 	}
 }
 
-func TestGetAngleBetween(t *testing.T) {
+func TestUtilsGetAngleBetween(t *testing.T) {
 	vector := New(2.0, 4.0)
 	other := New(5.0, 12.0)
 	angle_between := GetAngleBetween(vector, other)
@@ -38,12 +38,120 @@ func TestGetAngleBetween(t *testing.T) {
 	}
 }
 
-func TestGetDistance(t *testing.T) {
+func TestUtilsGetDistance(t *testing.T) {
 	vector := New(2.0, 4.0)
 	other := New(5.0, 12.0)
 	distance := GetDistance(vector, other)
 
 	if distance != 8.54400374531753 {
 		t.Error("Distance between vector and other happens to be:", distance)
+	}
+}
+
+func TestUtilsCollect(t *testing.T) {
+	vector := New(2.0, 4.0)
+	other := New(3.0, 5.0)
+	result := Collect(vector, other)
+
+	if result.X != 5.0 && result.Y != 9.0 {
+		t.Error("Vector + Other vector gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("Collect did not return a copy object")
+	}
+}
+
+func TestUtilsCollectToFloat64(t *testing.T) {
+	vector := New(2.0, 4.0)
+	result := CollectToFloat64(vector, 3.0)
+
+	if result.X != 5.0 && result.Y != 6.0 {
+		t.Error("Vector + float64 gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("CollectToFloat64 did not return a copy object")
+	}
+}
+
+func TestUtilsSub(t *testing.T) {
+	vector := New(4.0, 4.0)
+	other := New(2.0, 3.0)
+	result := Sub(vector, other)
+
+	if result.X != 2.0 && result.Y != 1.0 {
+		t.Error("Vector - Other vector gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("Sub did not return a copy object")
+	}
+}
+
+func TestUtilsSubToFloat64(t *testing.T) {
+	vector := New(2.0, 4.0)
+	result := SubToFloat64(vector, 1.0)
+
+	if result.X != 1.0 && result.Y != 3.0 {
+		t.Error("Vector - float64 gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("SubToFloat64 did not return a copy object")
+	}
+}
+
+func TestUtilsMul(t *testing.T) {
+	vector := New(2.0, 4.0)
+	other := New(3.0, 5.0)
+	result := Mul(vector, other)
+
+	if result.X != 6.0 && result.Y != 20.0 {
+		t.Error("Vector * Other gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("Mul did not return a copy object")
+	}
+}
+
+func TestUtilsMulToFloat64(t *testing.T) {
+	vector := New(2.0, 4.0)
+	result := MulToFloat64(vector, 3.0)
+
+	if result.X != 6.0 && result.Y != 12.0 {
+		t.Error("Vector * Other gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("MulToFloat64 did not return a copy object")
+	}
+}
+
+func TestUtilsDiv(t *testing.T) {
+	vector := New(4.0, 8.0)
+	other := New(2.0, 4.0)
+	result := Div(vector, other)
+
+	if result.X != 2.0 && result.Y != 2.0 {
+		t.Error("Vector * Other gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("Div did not return a copy object")
+	}
+}
+
+func TestUtilsDivToFloat64(t *testing.T) {
+	vector := New(2.0, 4.0)
+	result := DivToFloat64(vector, 2.0)
+
+	if result.X != 1.0 && result.Y != 2.0 {
+		t.Error("Vector * Other gave X:", result.X, " Y:", result.Y)
+	}
+
+	if vector == result {
+		t.Error("DivToFloat64 did not return a copy object")
 	}
 }
